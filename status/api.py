51fb405e8bc932ced55b3bafabd4ae4d2520fb6b
Adrian Hannah <adrian@adrianhannah.net>
2019-12-10 18:26:59 +0000

    Added IncidentUpdateResource to be able to create new updates via the API.

--- status/api.py
+++ status/api.py
@@ -3,7 +3,7 @@ from tastypie.authorization import Authorization
 from tastypie.resources import NamespacedModelResource, fields, ALL, ALL_WITH_RELATIONS
 from django.contrib.auth.models import User #BUG: Import the correct user object from settings.py
 
-from .models import Incident, Status
+from .models import Incident, IncidentUpdate, Status
 
 import logging
 logger = logging.getLogger(__name__)
@@ -50,3 +50,31 @@ class IncidentResource(ReadOnlyFieldNamespacedModelResource):
             'status': ALL_WITH_RELATIONS,
         }
 
+
+class IncidentUpdateResource(NamespacedModelResource):
+    status = fields.ForeignKey(StatusResource, 'status', full=True)
+    incident = fields.ForeignKey(IncidentResource, 'incident', null=True, blank=True, related_name='updates')
+
+    def hydrate(self, bundle):
+        # Force API to not blank out created field
+        if 'id' in bundle.data:
+            bundle.data['created'] = IncidentUpdate.objects.get(id=bundle.data['id']).created
+
+        u = User.objects.get(id=bundle.request.user.id)
+        bundle.obj.user = u
+
+        return bundle
+
+    class Meta:
+        queryset = IncidentUpdate.objects.all()
+        allowed_methods = ['get', 'post', 'delete']
+        resource_name = 'incidentupdate'
+        authentication = ApiKeyAuthentication()
+        authorization = Authorization()
+        always_return_data = True
+        filtering = {
+            'created': ALL,
+            'updated': ALL,
+            'status': ALL_WITH_RELATIONS,
+            'incident': ALL_WITH_RELATIONS,
+        }
